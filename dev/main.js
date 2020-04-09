5d73c45c2044518e1180db79cecece1840b1960f
chenkang <672247608@qq.com>
2020-04-09 12:07:29 +0800

    refactor(theme/default.js): rename github.js

--- dev/main.js
+++ dev/main.js
@@ -1,47 +1,9 @@
 import App from './App';
 import Vue from 'vue';
-import VueMarkdownEditor from '../src/codemirror-editor';
-import 'highlight.js/styles/github';
-import hljs from 'highlight.js/lib/highlight.js';
-import jsLanguage from 'highlight.js/lib/languages/javascript';
-import xmlLanguage from 'highlight.js/lib/languages/xml';
-import cssLanguage from 'highlight.js/lib/languages/css';
-import 'github-markdown-css';
-import bash from 'highlight.js/lib/languages/bash';
-import markdownItInstance from '@/utils/markdown-it';
+import VueMarkdownEditor from '@/codemirror-editor';
+import githubTheme from '@/theme/github';
 
-markdownItInstance.set({
-  html: true,
-  xhtmlOut: true,
-  breaks: true,
-  linkify: false,
-  typographer: true,
-});
-
-// js
-hljs.registerLanguage('js', jsLanguage);
-hljs.registerLanguage('javascript', jsLanguage);
-
-// xml, html
-hljs.registerLanguage('html', xmlLanguage);
-hljs.registerLanguage('xml', xmlLanguage);
-
-// css
-hljs.registerLanguage('css', cssLanguage);
-
-hljs.registerLanguage('bash', bash);
-
-VueMarkdownEditor.markdownIt.set({
-  highlight(str, lang) {
-    let res = VueMarkdownEditor.markdownIt.utils.escapeHtml(str);
-
-    if (lang && hljs.getLanguage(lang)) {
-      res = hljs.highlight(lang, str).value;
-    }
-
-    return `<pre class="language-${lang}"><code>${res}</code></pre>`;
-  },
-});
+VueMarkdownEditor.useTheme(githubTheme);
 
 Vue.use(VueMarkdownEditor);
 
