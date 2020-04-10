7d56efb605676eed77f0159ef9197cf9697bb0c9
chenkang <672247608@qq.com>
2020-04-10 11:01:52 +0800

    chore: release 1.0.2

--- src/base-editor.js
+++ src/base-editor.js
@@ -2,7 +2,7 @@
 import Component from './base-editor.vue';
 import { setGlobal } from '@/utils/global';
 
-const version = '1.0.1';
+const version = '1.0.2';
 
 const install = (Vue) => {
   Vue.component(Component.name, Component);
