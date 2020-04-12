c4834bfafd66e1dbce44d4650cc2c906883a6796
Lynette <672247608@qq.com>
2020-04-12 16:51:11 +0800

    chore: release 1.0.9

--- src/preview.js
+++ src/preview.js
@@ -2,7 +2,7 @@
 import Component from './preview.vue';
 import { getGlobal, setGlobal } from '@/utils/global';
 
-const version = '1.0.8';
+const version = '1.0.9';
 
 const install = (Vue) => {
   Vue.component(Component.name, Component);
