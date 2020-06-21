72664386b6f71e1f696153fbf28e5a947d478602
Lynette <672247608@qq.com>
2020-06-21 20:50:23 +0800

    chore: release 1.4.1-beta.0

--- src/preview.js
+++ src/preview.js
@@ -5,7 +5,7 @@ import Lang from '@/lang/';
 // font css
 import '@/assets/css/font';
 
-const version = '1.4.0';
+const version = '1.4.1-beta.0';
 
 const install = (Vue) => {
   Vue.component(Component.name, Component);
