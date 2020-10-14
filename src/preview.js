7a8db5d2f983d0bbfea772e1e821d56708ecaeda
chenkang <672247608@qq.com>
2020-10-14 17:52:45 +0800

    chore: release 2.0.1

--- src/preview.js
+++ src/preview.js
@@ -5,7 +5,7 @@ import Lang from '@/lang/';
 // font css
 import '@/assets/css/font';
 
-const version = '2.0.1-beta.0';
+const version = '2.0.1';
 
 const install = (Vue) => {
   Vue.component(Component.name, Component);
