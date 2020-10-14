df3c9f43babb4306a360339d53f09c4cb60e42a7
chenkang <672247608@qq.com>
2020-10-14 16:20:22 +0800

    chore: release 2.0.0

--- src/codemirror-editor.js
+++ src/codemirror-editor.js
@@ -5,7 +5,7 @@ import Lang from '@/lang/';
 // font css
 import '@/assets/css/font';
 
-const version = '1.4.11';
+const version = '2.0.0';
 
 const install = (Vue) => {
   Vue.component(Component.name, Component);
