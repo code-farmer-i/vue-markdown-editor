fd97736ce488ac42f411b8ec80365a1af4b9d624
chenkang <672247608@qq.com>
2020-10-21 16:52:38 +0800

    chore: release 2.1.1

--- src/base-editor.js
+++ src/base-editor.js
@@ -5,7 +5,7 @@ import Lang from '@/lang/';
 // font css
 import '@/assets/css/font';
 
-const version = '2.1.1-beta.0';
+const version = '2.1.1';
 
 const install = (Vue) => {
   Vue.component(Component.name, Component);
