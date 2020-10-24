6ac92181cac41fe1e7954d590354739270468dd7
Lynette <672247608@qq.com>
2020-10-24 17:31:54 +0800

    chore: release 2.1.2-beta.0

--- src/base-editor.js
+++ src/base-editor.js
@@ -5,7 +5,7 @@ import Lang from '@/lang/';
 // font css
 import '@/assets/css/font';
 
-const version = '2.1.1';
+const version = '2.1.2-beta.0';
 
 const install = (Vue) => {
   Vue.component(Component.name, Component);
