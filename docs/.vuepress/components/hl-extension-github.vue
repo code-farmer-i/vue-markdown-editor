562b3f2c3a326902615c54f9ca5a49185f8d820a
Lynette <672247608@qq.com>
2020-11-05 20:16:25 +0800

    docs: 1.5.1 docs update

---
+++ docs/.vuepress/components/hl-extension-github.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-md-editor v-model="text" :theme="theme" :themeConfig="themeConfig" height="500px" />
+</template>
+
+<script>
+import VMdEditor from '../../../lib/base-editor';
+import '../../../lib/style/base-editor.css';
+import json from 'highlight.js/lib/languages/json';
+import githubTheme from '../../../lib/theme/github.js';
+
+VMdEditor.use(githubTheme, {
+  extend(md, hljs) {
+    hljs.registerLanguage('json', json);
+  },
+});
+
+const text = `\`\`\`vue
+<template></template>
+
+<script>
+export default {}
+<\/script>
+\`\`\``;
+
+export default {
+  components: {
+    [VMdEditor.name]: VMdEditor,
+  },
+  data() {
+    this.theme = githubTheme;
+    this.themeConfig = {
+      codeHighlightExtensionMap: {
+        vue: 'html',
+      },
+    };
+
+    return {
+      text,
+    };
+  },
+};
+</script>
---
+++ docs/.vuepress/components/hl-extension-vuepress.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-md-editor v-model="text" :theme="theme" :themeConfig="themeConfig" height="500px" />
+</template>
+
+<script>
+import VMdEditor from '../../../lib/base-editor';
+import '../../../lib/style/base-editor.css';
+import vuepressTheme from '../../../lib/theme/vuepress.js';
+import 'prismjs/components/prism-json';
+
+const text = `\`\`\`vue
+<template></template>
+
+<script>
+export default {}
+<\/script>
+\`\`\``;
+
+export default {
+  components: {
+    [VMdEditor.name]: VMdEditor,
+  },
+  data() {
+    this.theme = vuepressTheme;
+    this.themeConfig = {
+      codeHighlightExtensionMap: {
+        vue: 'html',
+      },
+    };
+
+    return {
+      text,
+    };
+  },
+};
+</script>
