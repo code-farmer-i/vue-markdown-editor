7c8e2513ce01a9e4cf2336077b5f33c6f0432f29
672247608@qq.com <672247608@qq.com>
2021-05-10 10:05:26 +0800

    style: lang config

--- src/lang/pl-PL.js
+++ src/lang/pl-PL.js
@@ -1,104 +1,104 @@
 export default {
   undo: {
-    toolbar: 'Cofnij'
+    toolbar: 'Cofnij',
   },
   redo: {
-    toolbar: 'Powtórz'
+    toolbar: 'Powtórz',
   },
   clear: {
-    toolbar: 'Wyczyść wszystko'
+    toolbar: 'Wyczyść wszystko',
   },
   h: {
-    toolbar: 'Nagłówki'
+    toolbar: 'Nagłówki',
   },
   h1: {
     toolbar: 'Nagłówek 1',
-    placeholder: 'Nagłówek 1'
+    placeholder: 'Nagłówek 1',
   },
   h2: {
     toolbar: 'Nagłówek 2',
-    placeholder: 'Nagłówek 2'
+    placeholder: 'Nagłówek 2',
   },
   h3: {
     toolbar: 'Nagłówek 3',
-    placeholder: 'Nagłówek 3'
+    placeholder: 'Nagłówek 3',
   },
   h4: {
     toolbar: 'Nagłówek 4',
-    placeholder: 'Nagłówek 4'
+    placeholder: 'Nagłówek 4',
   },
   h5: {
     toolbar: 'Nagłówek 5',
-    placeholder: 'Nagłówek 5'
+    placeholder: 'Nagłówek 5',
   },
   h6: {
     toolbar: 'Nagłówek 6',
-    placeholder: 'Nagłówek 6'
+    placeholder: 'Nagłówek 6',
   },
   bold: {
     toolbar: 'Pogrubienie',
-    placeholder: 'Pogrubienie'
+    placeholder: 'Pogrubienie',
   },
   italic: {
     toolbar: 'Kursywa',
-    placeholder: 'Kursywa'
+    placeholder: 'Kursywa',
   },
   strikethrough: {
     toolbar: 'Przekreślenie',
-    placeholder: 'Przekreślenie'
+    placeholder: 'Przekreślenie',
   },
   quote: {
     toolbar: 'Blok cytatu',
-    placeholder: 'Blok cytatu'
+    placeholder: 'Blok cytatu',
   },
   ul: {
     toolbar: 'Lista nieuporządkowana',
-    placeholder: 'Lista nieuporządkowana'
+    placeholder: 'Lista nieuporządkowana',
   },
   ol: {
     toolbar: 'Lista uporządkowana',
-    placeholder: 'Lista uporządkowana'
+    placeholder: 'Lista uporządkowana',
   },
   table: {
-    toolbar: 'Wstaw tabelę'
+    toolbar: 'Wstaw tabelę',
   },
   hr: {
-    toolbar: 'Linia'
+    toolbar: 'Linia',
   },
   link: {
     toolbar: 'Wstaw link',
-    descPlaceholder: 'Link'
+    descPlaceholder: 'Link',
   },
   image: {
-    toolbar: 'Wstaw grafikę'
+    toolbar: 'Wstaw grafikę',
   },
   imageLink: {
-    toolbar: 'Z linku'
+    toolbar: 'Z linku',
   },
   uploadImage: {
-    toolbar: 'Wyślij zdjęcie'
+    toolbar: 'Wyślij zdjęcie',
   },
   code: {
-    toolbar: 'Wstaw blok kodu'
+    toolbar: 'Wstaw blok kodu',
   },
   save: {
-    toolbar: 'Zapisz'
+    toolbar: 'Zapisz',
   },
   preview: {
     enabled: 'Pokaż podgląd',
-    disabled: 'Ukryj podgląd'
+    disabled: 'Ukryj podgląd',
   },
   toc: {
     title: 'Spis treści',
     enabled: 'Pokaż spis treści',
-    disabled: 'Ukryj spis treści'
+    disabled: 'Ukryj spis treści',
   },
   syncScroll: {
     enabled: 'Synchronizuj przesunięcie',
-    disabled: 'Nie synchronizuj przesunięcia'
+    disabled: 'Nie synchronizuj przesunięcia',
   },
   fullscreen: {
     enabled: 'Pełny ekran (Wciśnij ESC, aby opuścić)',
-    disabled: 'Opuść tryb pełnoekranowy'
-  }
-}
+    disabled: 'Opuść tryb pełnoekranowy',
+  },
+};
