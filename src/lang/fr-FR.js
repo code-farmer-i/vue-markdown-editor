0be3ae618bc607d62adf92cc83b3de8d447b603e
Louis Bailleau <louis.bailleau@free.fr>
2021-05-11 19:34:26 +0200

    Create fr-FR.js
    Just because French translations are missing.

---
+++ src/lang/fr-FR.js
@@ -0,0 +1,104 @@
+export default {
+  undo: {
+    toolbar: 'Annuler',
+  },
+  redo: {
+    toolbar: 'Restaurer',
+  },
+  clear: {
+    toolbar: 'Vider',
+  },
+  h: {
+    toolbar: 'Entêtes',
+  },
+  h1: {
+    toolbar: 'Entête 1',
+    placeholder: 'Entête 1',
+  },
+  h2: {
+    toolbar: 'Entête 2',
+    placeholder: 'Entête 2',
+  },
+  h3: {
+    toolbar: 'Entête 3',
+    placeholder: 'Entête 3',
+  },
+  h4: {
+    toolbar: 'Entête 4',
+    placeholder: 'Entête 4',
+  },
+  h5: {
+    toolbar: 'Entête 5',
+    placeholder: 'Entête 5',
+  },
+  h6: {
+    toolbar: 'Entête 6',
+    placeholder: 'Entête 6',
+  },
+  bold: {
+    toolbar: 'Gras',
+    placeholder: 'Gras',
+  },
+  italic: {
+    toolbar: 'Italique',
+    placeholder: 'Italique',
+  },
+  strikethrough: {
+    toolbar: 'Barré',
+    placeholder: 'Barré',
+  },
+  quote: {
+    toolbar: 'Citation',
+    placeholder: 'Citation',
+  },
+  ul: {
+    toolbar: 'List désordonnée',
+    placeholder: 'List désordonnée',
+  },
+  ol: {
+    toolbar: 'Liste ordonnée',
+    placeholder: 'Liste ordonnée',
+  },
+  table: {
+    toolbar: 'Insérer un tavleau',
+  },
+  hr: {
+    toolbar: 'Ligne',
+  },
+  link: {
+    toolbar: 'Insérer un lien',
+    descPlaceholder: 'Lien',
+  },
+  image: {
+    toolbar: 'Insérer une image',
+  },
+  imageLink: {
+    toolbar: 'Insérer un lien',
+  },
+  uploadImage: {
+    toolbar: 'Téléverser une image',
+  },
+  code: {
+    toolbar: 'Insérer un bloc de code',
+  },
+  save: {
+    toolbar: 'Enregister',
+  },
+  preview: {
+    enabled: 'Activer la prévisualisation',
+    disabled: 'Désactiver la prévisualisation',
+  },
+  toc: {
+    title: 'Table des matières',
+    enabled: 'Activer la table des matières',
+    disabled: 'Désactiver la table des matières',
+  },
+  syncScroll: {
+    enabled: 'Activer le défilement synchronisé',
+    disabled: 'Désactiver le défilement synchronisé',
+  },
+  fullscreen: {
+    enabled: 'Plein écran (Pressez ESC pour quitter)',
+    disabled: 'Quitter le plein écran',
+  },
+};
