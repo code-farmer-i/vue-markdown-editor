91fcb093c1af30133884095ace3306a04d5b94a7
672247608@qq.com <672247608@qq.com>
2021-05-21 18:04:20 +0800

    chore: release 1.6.5

--- src/codemirror-editor.js
+++ src/codemirror-editor.js
@@ -5,7 +5,7 @@ import Lang from '@/lang/';
 // font css
 import '@/assets/css/font';
 
-const version = '1.6.4';
+const version = '1.6.5';
 
 const install = (Vue) => {
   Vue.component(Component.name, Component);
