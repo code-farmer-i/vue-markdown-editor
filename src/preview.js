44019e6f422ddc24c1a830534c46702e7c84f99c
672247608@qq.com <672247608@qq.com>
2021-05-28 10:50:49 +0800

    chore: release 2.3.0

--- src/preview.js
+++ src/preview.js
@@ -2,7 +2,7 @@
 import Component from './preview.vue';
 import xss from '@/utils/xss/index';
 
-const version = '2.2.6';
+const version = '2.3.0';
 
 const install = (app) => {
   app.component(Component.name, Component);
