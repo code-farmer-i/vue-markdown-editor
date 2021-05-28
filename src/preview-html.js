406942eddf21dbb2188365e9df9fb222ed625e12
672247608@qq.com <672247608@qq.com>
2021-05-28 12:04:59 +0800

    chore: release 1.7.1-beta.0

--- src/preview-html.js
+++ src/preview-html.js
@@ -3,7 +3,7 @@ import Component from './preview-html.vue';
 // font css
 import '@/assets/css/font';
 
-const version = '1.7.0';
+const version = '1.7.1-beta.0';
 
 const install = (Vue) => {
   Vue.component(Component.name, Component);
