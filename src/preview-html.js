1944c5835ab764bc26726a4c29c8b172e9e87f0b
672247608@qq.com <672247608@qq.com>
2021-06-02 10:12:33 +0800

    chore: release 2.3.3-beta.0

--- src/preview-html.js
+++ src/preview-html.js
@@ -3,7 +3,7 @@ import Component from './preview-html.vue';
 // font css
 import '@/assets/css/font';
 
-const version = '2.3.2';
+const version = '2.3.3-beta.0';
 
 const install = (app) => {
   app.component(Component.name, Component);
