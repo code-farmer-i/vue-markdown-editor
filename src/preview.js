41364c7d26e5d1c353efde58b8da857ba1f213f7
672247608@qq.com <672247608@qq.com>
2021-06-03 15:44:09 +0800

    chore: release 1.7.3

--- src/preview.js
+++ src/preview.js
@@ -5,7 +5,7 @@ import Lang from '@/lang/';
 // font css
 import '@/assets/css/font';
 
-const version = '1.7.2';
+const version = '1.7.3';
 
 const install = (Vue) => {
   Vue.component(Component.name, Component);
