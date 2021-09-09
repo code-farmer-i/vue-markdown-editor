87564230bc047a3bf3316d527100cc7df6487d6d
672247608@qq.com <672247608@qq.com>
2021-09-09 16:19:47 +0800

    chore: release 2.3.9

--- src/preview-html.js
+++ src/preview-html.js
@@ -3,7 +3,7 @@ import Component from './preview-html.vue';
 // font css
 import '@/assets/css/font';
 
-const version = '2.3.9-beta.0';
+const version = '2.3.9';
 
 const install = (app) => {
   app.component(Component.name, Component);
