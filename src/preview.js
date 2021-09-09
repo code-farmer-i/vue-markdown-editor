f4db0ab782ee63b6bb209bb9fcc9f8f156a5d3a1
672247608@qq.com <672247608@qq.com>
2021-09-09 17:12:55 +0800

    chore: release 2.3.10

--- src/preview.js
+++ src/preview.js
@@ -4,7 +4,7 @@ import xss from '@/utils/xss/index';
 // font css
 import '@/assets/css/font';
 
-const version = '2.3.9';
+const version = '2.3.10';
 
 const install = (app) => {
   app.component(Component.name, Component);
