f2b8be0f30126a04b46a436cffb80973cb74a035
672247608@qq.com <672247608@qq.com>
2021-10-11 17:25:24 +0800

    chore: release 2.3.11

--- src/base-editor.js
+++ src/base-editor.js
@@ -4,7 +4,7 @@ import xss from '@/utils/xss/index';
 // font css
 import '@/assets/css/font';
 
-const version = '2.3.10';
+const version = '2.3.11';
 
 const install = (app) => {
   app.component(Component.name, Component);
