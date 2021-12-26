4ff34887056159255858b6d83db6575cabcda799
0biWanKenobi <metal.rulesall@gmail.com>
2021-12-26 13:12:50 +0100

    chore(deps): upgrade devDependencies

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,24 +1,30 @@
 const path = require('path');
-const merge = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const getBaseConfig = require('./webpack.base');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(getBaseConfig(), {
+  mode: 'development',
   entry: {
     dev: './dev/main.js',
   },
   devServer: {
     open: true,
-    progress: true,
-    host: '0.0.0.0',
-    stats: 'errors-only',
-    disableHostCheck: true,
+    host: '127.0.0.1',
+    allowedHosts: 'all',
+    client: {
+      progress: true,
+    },
+    devMiddleware: {
+      stats: 'errors-only',
+    },
   },
   output: {
     path: path.join(__dirname, '../dev/dist'),
     publicPath: '/',
     chunkFilename: 'async_[name].js',
   },
+  devtool: 'source-map',
   optimization: {
     splitChunks: {
       cacheGroups: {
