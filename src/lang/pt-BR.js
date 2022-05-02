1c2b84fd47df32f0630d84dcc0722a3b500447c5
Matheus Bozetti <matheusbzt@gmail.com>
2022-05-02 17:08:05 -0400

    chore: add pt-br editor lang

---
+++ src/lang/pt-BR.js
@@ -0,0 +1,104 @@
+export default {
+  undo: {
+    toolbar: 'Desfazer',
+  },
+  redo: {
+    toolbar: 'Refazer',
+  },
+  clear: {
+    toolbar: 'Limpar',
+  },
+  h: {
+    toolbar: 'Títulos',
+  },
+  h1: {
+    toolbar: 'Título 1',
+    placeholder: 'Título 1',
+  },
+  h2: {
+    toolbar: 'Título 2',
+    placeholder: 'Título 2',
+  },
+  h3: {
+    toolbar: 'Título 3',
+    placeholder: 'Título 3',
+  },
+  h4: {
+    toolbar: 'Título 4',
+    placeholder: 'Título 4',
+  },
+  h5: {
+    toolbar: 'Título 5',
+    placeholder: 'Título 5',
+  },
+  h6: {
+    toolbar: 'Título 6',
+    placeholder: 'Título 6',
+  },
+  bold: {
+    toolbar: 'Negrito',
+    placeholder: 'Negrito',
+  },
+  italic: {
+    toolbar: 'Itálico',
+    placeholder: 'Itálico',
+  },
+  strikethrough: {
+    toolbar: 'Riscar',
+    placeholder: 'Riscar',
+  },
+  quote: {
+    toolbar: 'Citação',
+    placeholder: 'Citação',
+  },
+  ul: {
+    toolbar: 'Lista não ordenada',
+    placeholder: 'Lista não ordenada',
+  },
+  ol: {
+    toolbar: 'Lista Ordenada',
+    placeholder: 'Lista Ordenada',
+  },
+  table: {
+    toolbar: 'Inserir tabela',
+  },
+  hr: {
+    toolbar: 'Inserir linha',
+  },
+  link: {
+    toolbar: 'Inserir link',
+    descPlaceholder: 'Link',
+  },
+  image: {
+    toolbar: 'Inserir imagem',
+  },
+  imageLink: {
+    toolbar: 'Inserir link da imagem',
+  },
+  uploadImage: {
+    toolbar: 'Enviar imagem',
+  },
+  code: {
+    toolbar: 'Inserir bloco de código',
+  },
+  save: {
+    toolbar: 'Salvar',
+  },
+  preview: {
+    enabled: 'Ativar pré-visualização',
+    disabled: 'Desativar pré-visualização',
+  },
+  toc: {
+    title: 'Diretório de navegação',
+    enabled: 'Ativar toc',
+    disabled: 'Desativar toc',
+  },
+  syncScroll: {
+    enabled: 'Ativar rolagem sincronizada',
+    disabled: 'Desativar rolagem sincronizada',
+  },
+  fullscreen: {
+    enabled: 'Tela cheia (ESC para sair)',
+    disabled: 'Sair de tela cheia',
+  },
+};
