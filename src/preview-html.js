54cd8b3556480e1dc44df5a9210197575667e424
672247608@qq.com <672247608@qq.com>
2023-07-25 16:14:37 +0800

    chore: release 2.3.16

--- src/preview-html.js
+++ src/preview-html.js
@@ -3,7 +3,7 @@ import Component from './preview-html.vue';
 // font css
 import '@/assets/css/font';
 
-const version = '2.3.15';
+const version = '2.3.16';
 
 const install = (app) => {
   app.component(Component.name, Component);
