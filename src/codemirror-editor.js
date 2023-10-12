4638dd37d416f5204528a0fb816af18fc4d2a2e2
672247608@qq.com <672247608@qq.com>
2023-10-12 17:41:36 +0800

    chore: release 2.3.18-beta.0

--- src/codemirror-editor.js
+++ src/codemirror-editor.js
@@ -4,7 +4,7 @@ import xss from '@/utils/xss/index';
 // font css
 import '@/assets/css/font';
 
-const version = '2.3.17';
+const version = '2.3.18-beta.0';
 
 const install = (app) => {
   app.component(Component.name, Component);
--- src/preview-html.js
+++ src/preview-html.js
@@ -3,7 +3,7 @@ import Component from './preview-html.vue';
 // font css
 import '@/assets/css/font';
 
-const version = '2.3.17';
+const version = '2.3.18-beta.0';
 
 const install = (app) => {
   app.component(Component.name, Component);
