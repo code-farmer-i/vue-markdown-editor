167698b6afd2eb0ac989d82de9f38f839776c582
672247608@qq.com <672247608@qq.com>
2023-11-09 11:07:38 +0800

    fix(mermaid): render error

--- src/plugins/mermaid/creator.js
+++ src/plugins/mermaid/creator.js
@@ -24,18 +24,7 @@ export default function creator(mermaid) {
 
     if (!eles.length || this.isEditMode) return;
 
-    let parseSuccess = false;
-    eles.forEach((ele) => {
-      try {
-        parseSuccess = mermaid.parse(ele.innerText);
-      } catch (e) {
-        if (!e.str) {
-          console.log(e);
-        }
-      }
-
-      if (parseSuccess) mermaid.init(null, ele);
-    });
+    mermaid.init(null, eles);
   }
 
   return function createMermaidPlugin({ mermaidInitializeOptions = {} } = {}) {
